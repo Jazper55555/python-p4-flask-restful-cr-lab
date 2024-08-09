e8ad7e0c5cc0c8c88cecdcf82aa4cfb0f44c718c
Jazper <jazpersaldana@yahoo.com>
2024-08-08 19:26:35 -0500

    flask RESTful create API lab complete

--- server/app.py
+++ server/app.py
@@ -17,11 +17,36 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+        response_dict = [plant.to_dict() for plant in Plant.query.all()]
+
+        return make_response(response_dict, 200)
+    
+    def post(self):
+        new_plant = Plant(
+            name = request.get_json()['name'],
+            image = request.get_json()['image'],
+            price = request.get_json()['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_body = new_plant.to_dict()
+
+        return make_response(response_body, 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+
+    def get(self, id):
+        single_plant = Plant.query.filter_by(id=id).first().to_dict()
         
+        return make_response(single_plant, 200)
+    
+api.add_resource(PlantByID, '/plants/<int:id>')    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
